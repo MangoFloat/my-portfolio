c67ce603e6095b92732a95019277b713f422cd27
June Jaictin <jaictinjune@gmail.com>
2019-08-07 19:03:48 +0100

    Simple Header & Footer
    - Baby steps.

--- src/components/header.js
+++ src/components/header.js
@@ -2,51 +2,10 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-import { StickyContainer, Sticky } from 'react-sticky'
-
 const Header = ({ siteTitle }) => (
-  <StickyContainer>
-    <Sticky>
-      {({
-          style,
-
-          // the following are also available but unused in this example
-          isSticky,
-          wasSticky,
-          distanceFromTop,
-          distanceFromBottom,
-          calculatedHeight
-        }) => (
-        <header
-          style={{
-            background: `#1d1d1d`,
-            marginBottom: `1.45rem`,
-          }}
-        >
-          <div
-            style={{
-              margin: `0 auto`,
-              maxWidth: 960,
-              padding: `1.45rem 1.0875rem`,
-            }}
-          >
-            <h1 style={{ margin: 0 }}>
-              <Link
-                to="/"
-                style={{
-                  color: `white`,
-                  textDecoration: `none`,
-                }}
-              >
-                {siteTitle}
-              </Link>
-            </h1>
-          </div>
-        </header>
-      )}
-    </Sticky>
-
-  </StickyContainer>
+  <div>
+    {siteTitle}
+  </div>
 )
 
 Header.propTypes = {
--- src/pages/test.js
+++ src/pages/test.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { Fragment } from "react"
+import Header from "../components/layouts/Header"
+import Footer from "../components/layouts/Footer"
+
 import TicTacToe from "../components/tic-tac-toe"
 
-const Test = () => (
-  <div>
-    <TicTacToe/>
-  </div>
-)
+class Test extends React.Component {
+  render() {
+    return <Fragment>
+      <Header/>
+      <TicTacToe/>
+
+      <Footer/>
+    </Fragment>
+  }
+
+}
 
 export default Test
