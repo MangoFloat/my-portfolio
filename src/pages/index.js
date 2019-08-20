eb30e94852908bb43dee30d551c27ba826fef0c9
June Jaictin <jaictinjune@gmail.com>
2019-08-21 00:38:20 +0100

    Added background colour.
    - Clicking links at top scrolls to projects with correct offset.
    TODO: 
    - Style header links depending on which element is being viewed.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,21 @@ import React, { Fragment } from 'react'
 import Header from '../components/layouts/Header'
 import Footer from '../components/layouts/Footer'
 
-import Home from '../components/home'
+import About from '../components/about'
+import Projects from '../components/projects'
+import Skills from '../components/skills'
+import Contacts from '../components/contact'
+
 import '../css/main.css'
 
 class IndexPage extends React.Component {
   render() {
     return <Fragment>
       <Header CurrentPage='home'/>
-      <Home/>
+      <About id='home'/>
+      <Projects id='projects'/>
+      <Skills id='skills'/>
+      <Contacts id='contacts'/>
       <Footer/>
     </Fragment>
   }
