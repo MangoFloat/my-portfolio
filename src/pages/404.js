425e83673ebebb612d99894a12dbce9783731d8c
June Jaictin <jaictinjune@gmail.com>
2019-10-07 19:24:18 +0100

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit ad729cbe10f2960c144827d6d54f519b62e18926.

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react"
+
+class PageNotFound extends React.Component {
+  render() {
+    return <Fragment>
+      <div>
+        Page not found...
+      </div>
+    </Fragment>
+  }
+
+}
+
+export default PageNotFound
